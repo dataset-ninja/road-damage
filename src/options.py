9788b868765506643d1ea22166c3b180d5a23add
almaz <almazgimaev@gmail.com>
2023-06-16 03:18:34 +0300

    add new options

--- src/options.py
+++ src/options.py
@@ -47,7 +47,14 @@ PREVIEWS_IS_DETECTION_TASK: bool = IS_DETECTION_TASK
 ###############################################################
 ### * Set up visualization params for ClassesPreview class ####
 CLASSES_PREVIEW_ROW_HEIGHT: int = 400
-CLASSES_PREVIEW_PADDINGS: dict = {"top": "100px", "bottom": "100px", "left": "100px", "right": "100px"}
+CLASSES_PREVIEW_ROWS: int = 1
+CLASSES_PREVIEW_GAP: int = None
+CLASSES_PREVIEW_PADDINGS: dict = {
+    "top": "100px",
+    "bottom": "100px",
+    "left": "100px",
+    "right": "100px",
+}
 # default {"top": "10%", "bottom": "10%", "left": "10%", "right": "10%"}
 # set % or px as string values (e.i. "10%" or "10px")
 ###############################################################
@@ -113,6 +120,8 @@ def get_stats_options():
         "ClassesPreview": {
             "row_height": CLASSES_PREVIEW_ROW_HEIGHT,
             "pad": CLASSES_PREVIEW_PADDINGS,
+            "rows": CLASSES_PREVIEW_ROWS,
+            "gap": CLASSES_PREVIEW_GAP,
         },
         "ClassesHeatmaps": {
             "draw_style": DRAW_STYLE,
