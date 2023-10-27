f0876889dbcafa7532765ca2e9f1dab03de62cef
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-10-27 18:10:05 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -56,6 +56,7 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 PAPER: Optional[str] = None
 CITATION_URL: Optional[str] = None
 AUTHORS: Optional[List[str]] = ["Alvaro Basily"]
+AUTHORS_CONTACTS: Optional[List[str]] = ["https://github.com/alvarobasilys", "https://www.linkedin.com/in/alvarobasilys/"]
 
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
 ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
@@ -103,6 +104,7 @@ def get_settings():
     settings["paper"] = PAPER
     settings["citation_url"] = CITATION_URL
     settings["authors"] = AUTHORS
+    settings["authors_contacts"] = AUTHORS_CONTACTS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
     settings["slytagsplit"] = SLYTAGSPLIT
